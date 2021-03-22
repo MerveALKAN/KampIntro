22ca111ee28e2b49e4eb4c1386877cabdb40af51
MerveALKAN <62889546+MerveALKAN@users.noreply.github.com>
2021-03-23 00:41:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,51 @@
 ﻿using System;
 
-namespace KampIntro
+namespace ClassIntro
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            //type safety 
-            string kategoriEtiketi = "Kategoriler";
-            int ogrenciSayisi = 32000;
-            double faizOrani = 1.45;
-            bool sistemeGirisYapilmisMi = true;
-            double dolarDun = 7.35;
-            double dolarBugun = 7.45;
+            string adi = "Merve";
+            int yas = 23;
 
-            if (dolarDun > dolarBugun)
-            {
-                Console.WriteLine("Azalış Butonu");
-            }
-            else if (dolarDun < dolarBugun)
-            {
-                Console.WriteLine("Artış Butonu");
-            }
-            else
-            {
-                Console.WriteLine("Değişmedi Butonu");
-            }
 
-                if (sistemeGirisYapilmisMi == true)
-            {
-                Console.WriteLine("Kullanıcı ayarları Butonu");
-            }
-            else
-            {
-                Console.WriteLine("Giriş Yap Butonu");
-            }
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 80;
 
-            Console.WriteLine(kategoriEtiketi);
 
-          
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOrani = 78;
+
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Java";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 76;
+
+            // Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : "+kurs.Egitmen);
+            }
+                
         }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
     }
 }

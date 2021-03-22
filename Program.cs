c0b453687e2d4a73cfdc2f1459df281ff0f9b0a9
MerveALKAN <62889546+MerveALKAN@users.noreply.github.com>
2021-03-23 00:44:17 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,51 +1,15 @@
 ﻿using System;
 
-namespace Metotlar
+namespace Matematik
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string urunAdi = "Elma";
-            double fiyati = 15;
-            string aciklama = "Amasya elması";
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5,6);
 
-            string[] meyveler = new string[] { };
-
-            Urun urun1 = new Urun();
-            urun1.Adi = "Elma";
-            urun1.Fiyati = 15;
-            urun1.Aciklama = "Amasya elması";
-            
-            Urun urun2 = new Urun();
-            urun2.Adi = "Karpuz";
-            urun2.Fiyati = 80;
-            urun2.Aciklama = "Diyarbakır karpuzu";
-
-            Urun[] urunler = new Urun[] { urun1, urun2 };
-
-            foreach (Urun urun in urunler)
-            {
-
-                Console.WriteLine(urun.Adi);
-                Console.WriteLine(urun.Fiyati);
-                Console.WriteLine(urun.Aciklama);
-                Console.WriteLine("-----------------------------");
-            }
-
-            Console.WriteLine("--------------Metotlar------------");
-
-
-            //instance - örnek
-            //encapsulation
-            SepetManager sepetManager = new SepetManager();
-            sepetManager.Ekle(urun1);
-            sepetManager.Ekle(urun2);
-
-            sepetManager.Ekle2("Armut", "Taze Armut", 12, 100);
-            sepetManager.Ekle2("Elma", "Yeşil Elma", 12, 50);
-            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu ", 12, 30);
-
+            dortIslem.Topla(6,9); 
         }
     }
 }

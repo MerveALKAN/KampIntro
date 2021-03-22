b354df6cfabd7a950ebcdab5be1746619f7eecb6
MerveALKAN <62889546+MerveALKAN@users.noreply.github.com>
2021-03-23 00:46:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 
-namespace Matematik
+namespace Donguler
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DortIslem dortIslem = new DortIslem();
-            dortIslem.Topla(5,6);
 
-            dortIslem.Topla(6,9); 
+
+            string kurs1 = "Yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "programlamaya başlangıç için temel kurs";
+            string kurs3 = "java";
+            string kurs4 = "python";
+            string kurs5 = "C#";
+
+            Console.WriteLine(kurs1); 
+            Console.WriteLine(kurs2); 
+            Console.WriteLine(kurs3); 
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+
+
+            //array -dizi 
+            string [] kurslar = new string[] { "Yazılım geliştirici yetiştirme kampı", 
+                "programlamaya başlangıç için temel kurs" ,
+            "java" , "python", "C#" };
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+    
+            foreach (string kurs in kurslar)
+                {
+                    Console.WriteLine(kurs);
+                }
+            
+            Console.WriteLine("sayfasonu - footer");
         }
     }
 }
